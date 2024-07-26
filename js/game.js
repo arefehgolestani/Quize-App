6fb5f19751959efd4304b67cd8c099d0a6528a4d
Arefeh <agolestani74@gmail.com>
2024-07-27 00:20:34 +0330

    get data from api in game.js file and create a module for formatData function.

---
+++ js/game.js
@@ -0,0 +1,35 @@
+import formatData from "./helper";
+const loader = document.getElementById("loader");
+const container = document.getElementById("container");
+const URL =
+  "https://opentdb.com/api.php?amount=10&difficulty=medium&type=multiple";
+let formattedData = null;
+
+// const formatData = (questionData) => {
+//   //   console.log(questionData);
+//   const result = questionData.map((item) => {
+//     const questionObject = { question: item.question };
+//     const answers = [...item.incorrect_answers];
+//     const correctAnswerIndex = Math.floor(Math.random() * 4);
+//     answers.splice(correctAnswerIndex, 0, item.correct_answer);
+//     questionObject.answers = answers;
+//     questionObject.correctAnswerIndex = correctAnswerIndex;
+//     return questionObject;
+//   });
+//   return result;
+// };
+
+const fetchData = async () => {
+  const response = await fetch(URL);
+  const json = await response.json();
+  formattedData = formatData(json.results);
+  console.log(formattedData);
+  start();
+};
+
+const start = () => {
+  loader.style.display = "none";
+  container.style.display = "block";
+};
+
+window.addEventListener("load", fetchData);
