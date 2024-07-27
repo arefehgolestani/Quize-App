8346e8c8707ddb87e1af8c808bf2f9aaffbf1926
Arefeh <agolestani74@gmail.com>
2024-07-27 17:57:30 +0330

    added 10 score for each correct answer and fixed checkAnswer function's bug.

--- js/game.js
+++ js/game.js
@@ -3,11 +3,15 @@ const loader = document.getElementById("loader");
 const container = document.getElementById("container");
 const questionText = document.getElementById("question-text");
 const answerList = document.querySelectorAll(".answer-text");
+const scoreText = document.getElementById("score");
+const CORRECT_BONUS = 10;
 const URL =
   "https://opentdb.com/api.php?amount=10&difficulty=medium&type=multiple";
 let formattedData = null;
 let questionIndex = 0;
 let correctAnswer = null;
+let score = 0;
+let isAccepted = true;
 
 // const formatData = (questionData) => {};
 
@@ -27,6 +31,7 @@ const showQuestion = () => {
   const { question, answers, correctAnswerIndex } =
     formattedData[questionIndex];
   correctAnswer = correctAnswerIndex;
+  console.log(correctAnswer);
   questionText.innerText = question;
   answerList.forEach((button, index) => {
     button.innerText = answers[index];
@@ -34,9 +39,13 @@ const showQuestion = () => {
 };
 
 const checkAnswer = (event, index) => {
+  if (!isAccepted) return;
+  isAccepted = false;
   const isCorrect = index === correctAnswer ? true : false;
   if (isCorrect) {
     event.target.classList.add("correct");
+    score += CORRECT_BONUS;
+    scoreText.innerText = score;
   } else {
     event.target.classList.add("incorrect");
     answerList[correctAnswer].classList.add("correct");
