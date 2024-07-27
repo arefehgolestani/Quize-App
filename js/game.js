5e15055267d6cc51aafda518b98cb54a35b767f7
Arefeh <agolestani74@gmail.com>
2024-07-27 17:06:41 +0330

    added js code for : show question in questionbox and show correct or incorrect answer to user . added styles for correct and incorrect answer in game.css

--- js/game.js
+++ js/game.js
@@ -1,35 +1,49 @@
-import formatData from "./helper";
+import formatData from "./helper.js";
 const loader = document.getElementById("loader");
 const container = document.getElementById("container");
+const questionText = document.getElementById("question-text");
+const answerList = document.querySelectorAll(".answer-text");
 const URL =
   "https://opentdb.com/api.php?amount=10&difficulty=medium&type=multiple";
 let formattedData = null;
+let questionIndex = 0;
+let correctAnswer = null;
 
-// const formatData = (questionData) => {
-//   //   console.log(questionData);
-//   const result = questionData.map((item) => {
-//     const questionObject = { question: item.question };
-//     const answers = [...item.incorrect_answers];
-//     const correctAnswerIndex = Math.floor(Math.random() * 4);
-//     answers.splice(correctAnswerIndex, 0, item.correct_answer);
-//     questionObject.answers = answers;
-//     questionObject.correctAnswerIndex = correctAnswerIndex;
-//     return questionObject;
-//   });
-//   return result;
-// };
+// const formatData = (questionData) => {};
 
 const fetchData = async () => {
   const response = await fetch(URL);
   const json = await response.json();
   formattedData = formatData(json.results);
-  console.log(formattedData);
   start();
 };
 
 const start = () => {
+  showQuestion();
   loader.style.display = "none";
   container.style.display = "block";
 };
+const showQuestion = () => {
+  const { question, answers, correctAnswerIndex } =
+    formattedData[questionIndex];
+  correctAnswer = correctAnswerIndex;
+  questionText.innerText = question;
+  answerList.forEach((button, index) => {
+    button.innerText = answers[index];
+  });
+};
+
+const checkAnswer = (event, index) => {
+  const isCorrect = index === correctAnswer ? true : false;
+  if (isCorrect) {
+    event.target.classList.add("correct");
+  } else {
+    event.target.classList.add("incorrect");
+    answerList[correctAnswer].classList.add("correct");
+  }
+};
 
 window.addEventListener("load", fetchData);
+answerList.forEach((botton, index) => {
+  botton.addEventListener("click", (event) => checkAnswer(event, index));
+});
